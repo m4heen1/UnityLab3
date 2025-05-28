89f84e34087be79d245728735e027c246b8cabf2
Bhuiyan_Maheen <bhuim263@hsd.k12.or.us>
2025-05-28 12:40:17 -0700

    Basic Movement

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+    private float zBound = 6;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound); 
+        }
+    }
+}
